ff061a3b1177d8221024300e2a6a85d127a271ce
oldman <597800451@qq.com>
2022-11-25 20:44:53 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <div class="preloader position-fixed d-flex align-items-center justify-content-center">
+      <div class="block">
+        <div class="loader-image mb-20">
+          <img src="@/assets/images/preloader.png" alt="">
+        </div>
+        <h2 class="loader-text text-uppercase">
+          <span class="h3 font-weight-light mb-1">Welcome to</span>
+          <span class="font-weight-bold">STFIL ILO</span>
+        </h2>
+      </div>
+    </div>
     <i-header/>
     <router-view/>
     <i-footer></i-footer>
