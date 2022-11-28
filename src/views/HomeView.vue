1c9a729796e806c35fdc47f1e28f24cda36f2b52
oldman <597800451@qq.com>
2022-11-28 18:27:45 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <section class="banner bg-cover-bottom has-shapes bg-light-gray has-bg-brash bg-brash-bottom aos-init aos-animate"
-             style="background-image: url('./images/brushes/section-bottom.svg');"
+             style="background-image: url('./images/brushes/banner.svg');"
              data-aos="fade-in" data-aos-delay="150">
       <div class="container h-100">
         <div class="row justify-content-center">
@@ -15,8 +15,8 @@
     <section class="section-padding">
       <div class="container h-100">
         <div class="has-colored-text row justify-content-center colored-icon-box">
-          <div class="col-md-6 col-sm-6 mb-30">
-            <div class="bg-white shadow rounded p-30 aos-init aos-animate" data-aos="fade-up" data-aos-delay="50"
+          <div class="col-lg-6 col-md-12 col-12 mb-30">
+            <div class="bg-white shadow rounded p-2 p-lg-5 p-md-5 p-sm-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="50"
                  style="height: 100%;">
               <div class="d-flex justify-content-between mb-2 mb-lg-3">
                 <h5 class="text-black-500"></h5>
@@ -25,13 +25,13 @@
               <div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">总计</div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">15,000,000 X2Y2 (1.5%)</h5>
                   </div>
                 </div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">槽</div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">1,000</h5>
                   </div>
                 </div>
@@ -39,29 +39,29 @@
                   <div class="col-4">
                     每一份的价格
                   </div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">1.5 ETH</h5>
                   </div>
                 </div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">开始时间
                   </div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">2022/02/14 8:00:00 UTC</h5>
                   </div>
                 </div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">结束时间
                   </div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">2022/02/14 8:00:00 UTC</h5>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-sm-6 mb-30">
-            <div class="bg-white shadow rounded p-30 aos-init aos-animate" data-aos="fade-up" data-aos-delay="50"
+          <div class="col-lg-6 col-md-12 col-12 mb-30">
+            <div class="bg-white shadow rounded  p-2 p-lg-5 p-md-5 p-sm-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="50"
                  style="height: 100%;">
               <div class="d-flex justify-content-between mb-2 mb-lg-3">
                 <h5 class="text-black-500"></h5>
@@ -70,19 +70,19 @@
               <div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">FDO代币</div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">15,000 X2Y2</h5>
                   </div>
                 </div>
                 <div class="row align-items-center mb-2">
                   <div class="col-4">创始NFT</div>
-                  <div class="col-8">
+                  <div class="col-8 text-right text-md-right text-lg-left">
                     <h5 class="font-weight-bold">1/1000</h5>
                   </div>
                 </div>
                 <div class="row align-items-center mt-4 mb-2">
-                  <div class="col-4"></div>
-                  <div class="col-6">
+                  <div class="col-lg-4 col-md-4"></div>
+                  <div class="col-12 col-lg-8 col-md-8 text-right text-md-right text-lg-left">
                     <div class="d-inline-block">
                       <img class="img-fluid rounded-sm" src="@/assets/images/career/01.jpg" alt="">
                     </div>
@@ -104,33 +104,39 @@
     <section class="section-padding pt-0">
       <div class="container">
         <div class="alert alert-danger alert-pink p-lg-5">
-          <div class="row">
-            <div class="col-lg-3 col-md-12 col-sm-12 row">
-              <div class="col-lg-12 col-md-6 col-sm-6">
-                <h6>销售阶段</h6>
-              </div>
-              <div class="col-lg-12 col-md-6 col-sm-6 mt-lg-2">
-                <h5 class="font-weight-bold">已发射</h5>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-12">
-              <div class="col-lg-12 col-md-6">
-                <h6>白名单</h6>
-              </div>
-              <div class="col-lg-12 col-md-6 mt-lg-2">
-                <h5 class="font-weight-bold">0</h5>
+          <div class="row pt-lg-0 pt-sm-2 pb-sm-2 justify-content-center align-items-center">
+            <div class="col-lg-3 col-md-12 col-sm-12 mt-2 mt-lg-0">
+              <div class="row justify-content-center align-items-center">
+                <div class="col-lg-12 col-md-6 col-sm-6 col-6">
+                  <h6>销售阶段</h6>
+                </div>
+                <div class="col-lg-12 col-md-6 col-sm-6 mt-lg-2 col-6 text-right text-md-right text-lg-left">
+                  <h5 class="font-weight-bold">已发射</h5>
+                </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-12">
-              <div class="col-lg-12 col-md-6">
-                <h6>剩余份额</h6>
-              </div>
-              <div class="col-lg-12 col-md-6 mt-lg-2">
-                <h5 class="font-weight-bold">0/1000</h5>
+            <div class="col-lg-3 col-md-12 col-sm-12 mt-2 mt-lg-0">
+              <div class="row  justify-content-center align-items-center">
+                <div class="col-lg-12 col-md-6 col-sm-6 col-6">
+                  <h6>白名单</h6>
+                </div>
+                <div class="col-lg-12 col-md-6 col-sm-6 col-6 mt-lg-2 text-right text-md-right text-lg-left">
+                  <h5 class="font-weight-bold">0</h5>
+                </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-12 text-lg-right text-md-center text-sm-center">
-              <button disabled class="btn btn-primary" type="submit">已发射</button>
+            <div class="col-lg-3 col-md-12 mt-2 mt-lg-0">
+              <div class="row  justify-content-center align-items-center">
+                <div class="col-lg-12 col-md-6 col-sm-6 col-6">
+                  <h6>剩余份额</h6>
+                </div>
+                <div class="col-lg-12 col-md-6 col-sm-6 col-6 mt-lg-2 text-right text-md-right text-lg-left">
+                  <h5 class="font-weight-bold">0/1000</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-12 col-sm-12 text-lg-right text-md-center text-sm-center text-center mt-4 mt-lg-0">
+              <button disabled class="btn btn-primary btn-sm " type="submit">已发射</button>
             </div>
           </div>
         </div>
@@ -143,8 +149,8 @@
       <div class="container">
         <nav>
           <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active nav-link-none fs-5 font-weight-500" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">常见问题</button>
-            <button class="nav-link nav-link-none fs-5 font-weight-500" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">白名单</button>
+            <button class="text-black-400 nav-link active nav-link-none fs-5 font-weight-500" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">常见问题</button>
+            <button class="text-black-400 nav-link nav-link-none fs-5 font-weight-500" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">白名单</button>
           </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
